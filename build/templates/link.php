3b1ef782a6d23c4400c3fa1cdd1d2e290e2763d4
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-12-18 12:48:10 +0200

    Initial skeleton

---
+++ build/templates/link.php
@@ -0,0 +1,37 @@
+<?php
+$hardlink_files = array(
+);
+
+$symlink_files = array(
+	# OTP Plugin
+	'../liveupdate/plugins/system/oneclickaction/LICENSE.txt'
+												=> 'plugins/system/oneclickaction/LICENSE.txt',
+	'../liveupdate/plugins/system/oneclickaction/oneclickaction.php'
+												=> 'plugins/system/oneclickaction/oneclickaction.php',
+	'../liveupdate/plugins/system/oneclickaction/oneclickaction.xml'
+												=> 'plugins/system/oneclickaction/oneclickaction.xml',
+);
+
+$symlink_folders = array(
+	# Build files
+	'../buildfiles/bin'							=> 'build/bin',
+	'../buildfiles/buildlang'					=> 'build/buildlang',
+	'../buildfiles/phingext'					=> 'build/phingext',
+	'../buildfiles/tools'						=> 'build/tools',
+
+	# Component translation
+	'translations/component/backend/en-GB'		=> 'component/language/backend/en-GB',
+	'translations/component/frontend/en-GB'		=> 'component/language/frontend/en-GB',
+
+	# OTP Plugin
+	'../liveupdate/plugins/system/oneclickaction/sql'
+												=> 'plugins/system/oneclickaction/sql',
+	'../liveupdate/plugins/system/oneclickaction/language'
+												=> 'translations/plugins/system/oneclickaction',
+	# FOF
+	'../fof/fof'								=> 'component/fof',
+
+	# Akeeba Strapper
+	'../fof/strapper'							=> 'component/strapper',
+
+);
---
+++ component/backend/cmsupdate.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ *  @package    AkeebaCMSUpdate
+ *  @copyright  Copyright (c)2010-2013 Nicholas K. Dionysopoulos
+ *  @license    GNU General Public License version 3, or later
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+defined('_JEXEC') or die();
+
+// Load FOF
+include_once JPATH_ADMINISTRATOR.'/components/com_admintools/fof/include.php';
+if(!defined('FOF_INCLUDED') || !class_exists('FOFForm', true))
+{
+	JError::raiseError ('500', 'Your Akeeba CMS Update installation is broken; please re-install. Alternatively, extract the installation archive and copy the fof directory inside your site\'s libraries directory.');
+}
+
+// Access check, Joomla! 1.6 style.
+if (!JFactory::getUser()->authorise('core.manage', 'com_cmsupdate'))
+{
+	return JError::raiseError(403, JText::_('JERROR_ALERTNOAUTHOR'));
+}
+
+FOFDispatcher::getTmpInstance('com_cmsupdate')->dispatch();
